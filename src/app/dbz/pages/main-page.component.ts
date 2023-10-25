ea141ce73b7bdbc310569c96e6bef86d40f62424
Daniel <daestradam@unal.edu.co>
2023-10-24 19:21:05 -0500

    media queries

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -5,7 +5,8 @@ import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
-  templateUrl: './main-page.component.html'
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
 })
 
 export class MainPageComponent{
